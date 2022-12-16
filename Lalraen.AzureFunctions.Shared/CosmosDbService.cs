259883ebb1be52fef014fec06fef0cad3dc0ef21
Volodymyr Minenko <minenko.od@gmail.com>
2022-12-16 20:46:55 +0200

    Add project files.

---
+++ Lalraen.AzureFunctions.Shared/CosmosDbService.cs
@@ -0,0 +1,77 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Logging;
+
+namespace Lalraen.AzureFunctions.Shared
+{
+    public class CosmosDbService<T> where T : class, ITableEntity, new()
+    {
+        private readonly ILogger _logger;
+        private readonly TableClient _tableClient;
+        private readonly string _partitionKey;
+        private readonly string _tableName;
+
+        public CosmosDbService(ILogger logger, string connectionString, string tableName, string partitionKey)
+        {
+            _tableName = tableName;
+            _logger = logger;
+            _tableClient = new TableClient(connectionString, tableName);
+            _partitionKey = partitionKey;
+        }
+
+        public async Task Add(T entity)
+        {
+            await _tableClient
+                .AddEntityAsync(entity)
+                .ConfigureAwait(false);
+
+            _logger.LogDebug($"Entity with {entity.RowKey} row key added to {_tableName}");
+        }
+
+        public async Task<T?> Get(string rowKey)
+        {
+            var response = await _tableClient
+                .GetEntityAsync<T>(_partitionKey, rowKey)
+                .ConfigureAwait(false);
+
+            if (response.HasValue)
+            {
+                _logger.LogDebug($"Entity with {rowKey} row key  retrieved");
+
+                return response.Value;
+            }
+
+            return null;
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            var entities = await _tableClient
+                .QueryAsync<T>(x => x.PartitionKey == _partitionKey)
+                .ToListAsync()
+                .ConfigureAwait(false);
+
+            _logger.LogDebug($"Loaded {entities.Count} for partition key {_partitionKey}");
+
+            return entities;
+        }
+
+        public async Task Update(T entity)
+        {
+            await _tableClient
+                .UpdateEntityAsync(entity, ETag.All)
+                .ConfigureAwait(false);
+
+            _logger.LogDebug($"Entity with {entity.RowKey} row key updated in {_tableName}");
+        }
+
+        public async Task Delete(string rowKey)
+        {
+            await _tableClient
+                .DeleteEntityAsync(_partitionKey, rowKey)
+                .ConfigureAwait(false);
+
+            _logger.LogDebug($"Entity with {rowKey} row key deleted from {_tableName}");
+        }
+    }
+}
---
+++ Lalraen.AzureFunctions.Shared/TelegramBotService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Lalraen.AzureFunctions.Shared
+{
+    public class TelegramBotService
+    {
+        private readonly ILogger _logger;
+        private readonly ITelegramBotClient _telegramBotClient;
+
+        public TelegramBotService(ILogger logger, string botToken)
+        {
+            _logger = logger;
+            _telegramBotClient = new TelegramBotClient(botToken);
+        }
+
+        public async Task SendMessageAsync(ChatId chatId, string message)
+        {
+            CheckMessageString(message);
+
+            _logger.LogInformation(message);
+
+            await SendTextMessageWithAction(chatId, message);
+        }
+
+        public async Task SendMessageToAllClientsAsync(IEnumerable<ChatId> clientChatIds, string message)
+        {
+            CheckMessageString(message);
+
+            _logger.LogInformation(message);
+
+            foreach (var chatId in clientChatIds)
+            {
+                await SendTextMessageWithAction(chatId, message);
+            }
+        }
+
+        private async Task SendTextMessageWithAction(ChatId chatId, string message)
+        {
+            await _telegramBotClient.SendChatActionAsync(chatId, ChatAction.Typing);
+            await _telegramBotClient.SendTextMessageAsync(chatId, message);
+        }
+
+        private void CheckMessageString(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                const string ErrorMessage = $"{nameof(message)} has no content. String is empty.";
+
+                _logger.LogError(ErrorMessage);
+
+                throw new ArgumentNullException(nameof(message), ErrorMessage);
+            }
+        }
+    }
+}
